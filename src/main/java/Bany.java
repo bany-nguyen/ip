6f4c59ae54a4c05c79ee39fbed9ebdba517cedc3
Bany <phuctruong.ng@gmail.com>
2026-08-19 17:28:35 +0800

    Add Echo Command

--- src/main/java/Bany.java
+++ src/main/java/Bany.java
@@ -1,13 +1,30 @@
+import java.util.Scanner;
+
 public class Bany {
     public static void main(String[] args) {
         String banner = """
+                ____________________________________________________________
                  ____                      
                 | __ )  __ _ _ __  _   _ 
                 |  _ \\ / _` | '_ \\| | | |
                 | |_) | (_| | | | | |_| |
                 |____/ \\__,_|_| |_|\\__, |
                                    |___/ 
-                """;
+                Hello! I'm Bany.
+                What can I do for you?
+                ____________________________________________________________""";
         System.out.println(banner);
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            String line = scanner.nextLine();
+            System.out.println("____________________________________________________________");
+            if (line.equalsIgnoreCase("bye")) {
+                System.out.println("Bye. Hope to see you again soon!");
+                System.out.println("____________________________________________________________");
+                break;
+            }
+            System.out.println(line);
+            System.out.println("____________________________________________________________");
+        }
     }
 }
