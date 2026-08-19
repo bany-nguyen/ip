45951ae0fa5abf5c8ad3f6318215f3a325e70d9a
Bany <phuctruong.ng@gmail.com>
2026-08-19 17:02:52 +0800

    Change bot name to Bany

---
+++ src/main/java/Bany.java
@@ -0,0 +1,13 @@
+public class Bany {
+    public static void main(String[] args) {
+        String banner = """
+                 ____                      
+                | __ )  __ _ _ __  _   _ 
+                |  _ \\ / _` | '_ \\| | | |
+                | |_) | (_| | | | | |_| |
+                |____/ \\__,_|_| |_|\\__, |
+                                   |___/ 
+                """;
+        System.out.println(banner);
+    }
+}
